495cd13e94146c970d6c00e6c8297ca87e9f1dfb
Brent Lamb <blamb@fathomgame.com>
2016-09-19 20:32:42 -0700

    Added screen for generic ints

--- src/c/amount_screen.c
+++ src/c/amount_screen.c
@@ -2,14 +2,9 @@
 #include <@smallstoneapps/utils/macros.h>
 #include <@smallstoneapps/bitmap-loader/bitmap-loader.h>
 
-#include "duration_screen.h"
+#include "amount_screen.h"
 
-#include "timer.h"
-#include "common.h"
 #include "icons.h"
-
-#define MODE_MINUTES 0
-#define MODE_SECONDS 1
 
 static void window_load(Window* window);
 static void window_unload(Window* window);
@@ -18,21 +13,17 @@ static void layer_action_bar_click_config_provider(void *context);
 static void action_bar_layer_down_handler(ClickRecognizerRef recognizer, void *context);
 static void action_bar_layer_up_handler(ClickRecognizerRef recognizer, void *context);
 static void action_bar_layer_select_handler(ClickRecognizerRef recognizer, void *context);
-static void update_timer_length(void);
 
 static Window* s_window;
 static Layer* s_layer;
 static ActionBarLayer* s_action_bar;
-static uint32_t s_duration = 0;
+static uint8_t s_amount = 0;
+static char* s_display_str;
 static DurationCallback s_callback;
-
-static int s_mode = MODE_MINUTES;
-static int s_minutes = 0;
-static int s_seconds = 0;
 
 static GFont s_font_duration;
 
-void duration_screen_init(void) {
+void amount_screen_init(void) {
 	s_window = window_create();
 	window_set_window_handlers(s_window, (WindowHandlers) {
 		.load = window_load,
@@ -40,12 +31,11 @@ void duration_screen_init(void) {
 	});
 }
 
-void duration_screen_show(uint16_t duration, DurationCallback callback) {
-	s_duration = duration;
+void amount_screen_show(uint8_t amount, char* display_str, AmountCallback callback) {
+	s_amount = amount;
 	s_callback = callback;
+	s_display_str = display_str;
 	window_stack_push(s_window, true);
-	s_minutes = s_duration / 60;
-	s_seconds = s_duration % 60;
 	layer_mark_dirty(s_layer);
 }
 
@@ -73,25 +63,13 @@ static void window_unload(Window* window) {
 static void layer_update(Layer* me, GContext* ctx) {
 	graphics_context_set_text_color(ctx, GColorBlack);
 	graphics_context_set_stroke_color(ctx, GColorBlack);
+
+	graphics_draw_text(ctx, s_display_str, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(0, 0, PEBBLE_WIDTH - 16, 28), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 	
-	char summary_str[32];
-	timer_time_str(s_duration, summary_str, 32);
-	graphics_draw_text(ctx, summary_str, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(0, 0, PEBBLE_WIDTH - 16, 28), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
-	
-	char* time_str = "000";
-	char mode_str[16];
-	switch (s_mode) {
-		case MODE_MINUTES:
-			snprintf(time_str, 3, "%02d", s_minutes);
-			snprintf(mode_str, 16, s_minutes == 1 ? "MINUTE" : "MINUTES");
-		break;
-		case MODE_SECONDS:
-			snprintf(time_str, 3, "%02d", s_seconds);
-			snprintf(mode_str, 16, s_seconds == 1 ? "SECOND" : "SECONDS");
-		break;
-	}
+	char* num_str = "000";
+	snprintf(num_str, 3, "%02d", s_amount);
+
 	graphics_draw_text(ctx, time_str, s_font_duration, GRect(0, 27, PEBBLE_WIDTH - 16, 70), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
-	graphics_draw_text(ctx, mode_str, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0, 98, PEBBLE_WIDTH - 16, 18), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
 }
 
 static void layer_action_bar_click_config_provider(void *context) {
@@ -101,56 +79,23 @@ static void layer_action_bar_click_config_provider(void *context) {
 }
 
 static void action_bar_layer_down_handler(ClickRecognizerRef recognizer, void *context) {
-	switch (s_mode) {
-		case MODE_MINUTES:
-			s_minutes -= 1;
-			if (s_minutes < 0) {
-				s_minutes = 99;
-			}
-		break;
-		case MODE_SECONDS:
-			s_seconds -= 1;
-			if (s_seconds < 0) {
-				s_seconds = 60;
-			}
-		break;
+	s_amount -= 1;
+	if (s_amount < 0) {
+		s_amount = 99;
 	}
-	update_timer_length();
 	layer_mark_dirty(s_layer);
 }
 
 static void action_bar_layer_up_handler(ClickRecognizerRef recognizer, void *context) {
-	switch (s_mode) {
-		case MODE_MINUTES:
-			s_minutes += 1;
-			if (s_minutes >= 100) {
-				s_minutes = 99;
-			}
-		break;
-		case MODE_SECONDS:
-			s_seconds += 1;
-			if (s_seconds >= 60) {
-				s_seconds -= 60;
-			}
-		break;
+	s_amount += 1;
+	if (s_amount >= 100) {
+		s_amount = 0;
 	}
-	update_timer_length();
 	layer_mark_dirty(s_layer);
 }
 
 static void action_bar_layer_select_handler(ClickRecognizerRef recognizer, void *context) {
 	switch (s_mode) {
-		case MODE_MINUTES:
-			s_mode = MODE_SECONDS;
-		break;
-		case MODE_SECONDS:
-			s_callback(s_duration);
-			window_stack_pop(true);
-		break;
-	}
-	layer_mark_dirty(s_layer);
-}
-
-static void update_timer_length(void) {
-	s_duration = s_minutes * 60 + s_seconds;
+		s_callback(s_amount);
+		window_stack_pop(true);
 }
---
+++ src/c/amount_screen.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "timer.h"
+
+typedef void (*AmountCallback)(uint8_t amount);
+
+void amount_screen_init(void);
+void amount_screen_show(uint8_t amount, char* display_str, AmountCallback callback);
